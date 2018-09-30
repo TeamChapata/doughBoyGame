4f9e93c438eeee4a0cd2a21605fb65eab49770aa
Jorge Rambla Gonzalez <718911@unizar.es>
2018-09-30 03:08:08 +0200

    Añadida función para mostrar ataque y defensa Añadida mas documentacion en código

--- New Unity Project/Assets/Chapter01EventsController.cs
+++ New Unity Project/Assets/Chapter01EventsController.cs
@@ -25,9 +25,9 @@ public class Chapter01EventsController : MonoBehaviour
     // VARIABLES DE CONTROL DE EVENTOS
     [SerializeField] private bool vivo = true;                                // Variable de evento
     [SerializeField] private bool maxVida = true;                             // Variable de evento
-    [SerializeField] private bool ayudarJagri = false;
-    [SerializeField] private bool llaveCogida = false;
-    [SerializeField] private bool dragonMuerto = false;
+    [SerializeField] private bool ayudarJagri = false;                        // Variable de evento
+    [SerializeField] private bool llaveCogida = false;                        // Variable de evento
+    [SerializeField] private bool dragonMuerto = false;                       // Variable de evento
 
 
     // VARIABLES DE ESTADO
@@ -46,6 +46,8 @@ public class Chapter01EventsController : MonoBehaviour
         campoTexto = GameObject.Find(bocadillo).GetComponent<Text>();
         botonUno = GameObject.Find(op_a);
         botonDos = GameObject.Find(op_b);
+        campoAtaqueBox = GameObject.Find(campo_multiplicador_ataque).GetComponent<campoTexto>();
+        campoDefensaBox = GameObject.Find(campo_multiplicador_defensa).GetComponent<campoTexto>();
 
         //inicializacion de las vidas
         for (int n = 0; n < vidas.Length; n++)
@@ -157,12 +159,26 @@ public class Chapter01EventsController : MonoBehaviour
 
     public void nextEvent_a()
     {
-        riseEvent(estado, 0);
+        if (!vivo)
+        {
+            estado = -1;
+        }
+        else
+        {
+            riseEvent(estado, 0);
+        }
     }
 
     public void nextEvent_b()
     {
-        riseEvent(estado, 1);
+        if (!vivo)
+        {
+            estado = -1;
+        }
+        else
+        {
+            riseEvent(estado, 1);
+        }
     }
 
     private void setButtons(bool b1_interactiable, string b1_text, bool b2_interactiable, string b2_text)
@@ -175,6 +191,8 @@ public class Chapter01EventsController : MonoBehaviour
 
     public void riseEvent(int eventCode, int value)
     {
+        campoAtaqueBox.text = "" + (ataque + 1.0);
+        campoDefensaBox.text = "" + (defensa + 1.0);
         switch (eventCode)
         {
             case 0:
